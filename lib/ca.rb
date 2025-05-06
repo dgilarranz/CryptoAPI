d7130fe52a50aa538138eb56c69a6f6b13db2aa7
David Gilarranz Ortega <d.gilarranz1@usp.ceu.es>
2025-05-06 21:34:47 +0200

    CA - constructor properly generates a self-signed certificate

--- lib/ca.rb
+++ lib/ca.rb
@@ -1,9 +1,37 @@
 require 'openssl'
 
 class CA
-  attr_reader :private_key
+  attr_reader :private_key, :certificate
 
   def initialize(common_name)
+    # Create the key pair
     @private_key = OpenSSL::PKey::RSA.new 2048
+
+    # Create the certificate
+    @certificate = OpenSSL::X509::Certificate.new
+    @certificate.serial = 0
+    @certificate.version = 2
+    @certificate.not_before = Time.now
+    @certificate.not_after = certificate.not_before + 365 * 24 * 60 * 60
+
+    name = OpenSSL::X509::Name.parse "/CN=#{common_name}"
+    @certificate.subject = name
+    @certificate.issuer = name
+    @certificate.public_key = @private_key.public_key
+
+    # Add extensions
+    extension_factory = OpenSSL::X509::ExtensionFactory.new
+    extension_factory.subject_certificate = @certificate
+    extension_factory.issuer_certificate = @certificate
+
+    @certificate.add_extension \
+      extension_factory.create_extension('subjectKeyIdentifier', 'hash')
+    @certificate.add_extension \
+      extension_factory.create_extension('basicConstraints', 'CA:TRUE', true)
+    @certificate.add_extension \
+      extension_factory.create_extension('keyUsage', 'cRLSign,keyCertSign', true)
+
+    # Self-sign the certificate
+    @certificate.sign(@private_key, OpenSSL::Digest.new('SHA256'))
   end
 end
--- spec/ca_spec.rb
+++ spec/ca_spec.rb
@@ -13,6 +13,106 @@ describe 'A Certificate Authority (CA)' do
     # We check the length of the private key converted to pem. The resulting
     # string should be 1700, 1704 or 1708 characters in length
     # (lengths found empirically using irb and simulating possible values)
-    expect([1700, 1704, 1708]).to include ca.private_key.private_to_pem.length 
+    expect([1700, 1704, 1708]).to include ca.private_key.private_to_pem.length
+  end
+
+  it 'has a root certificate' do
+    ca = CA.new 'MyCA'
+
+    expect(ca.certificate).to be_a OpenSSL::X509::Certificate
+  end
+
+  it 'has a certificate with the requested Common Name (CN)' do
+    common_name = 'MyCA'
+    ca = CA.new common_name
+
+    expect(ca.certificate.subject.to_s.split('/')[1].split('=').last).to eq common_name
+  end
+
+  it 'has a root certificate with the serial number 0' do
+    ca = CA.new 'MyCA'
+
+    expected_serial = OpenSSL::BN.new 0
+    expect(ca.certificate.serial).to eq expected_serial
+  end
+
+  it 'has a v3 root certificate (RFC 5280)' do
+    ca = CA.new 'MyCA'
+
+    expect(ca.certificate.version).to be 2
+  end
+
+  it 'has a root certificate whose validity starts the moment it is created' do
+    # Mock calls to Time.now to return a fixed time
+    @time_now = Time.now
+    allow(Time).to receive(:now).and_return(@time_now)
+
+    ca = CA.new 'MyCA'
+
+    expect(ca.certificate.not_before.to_i).to eq @time_now.to_i
+  end
+
+  it 'has a root certificate valid for 365 days' do
+    # Mock calls to Time.now to return a fixed time
+    @time_now = Time.now
+    allow(Time).to receive(:now).and_return(@time_now)
+
+    ca = CA.new 'MyCA'
+
+    expect(ca.certificate.not_after.to_i).to eq (@time_now + 365 * 24 * 60 * 60).to_i
+  end
+
+  it 'has a self signed certificate (issuer = subject)' do
+    ca = CA.new 'MyCA'
+
+    expect(ca.certificate.issuer).to eq ca.certificate.subject
+  end
+
+  it 'has a certificate whose public key coincides with the created RSA key pair' do
+    ca = CA.new 'MyCA'
+
+    expect(ca.certificate.public_key.to_s).to eq ca.private_key.public_key.to_s
+  end
+
+  it 'has a certificate with a hash based Subject Key Identifier (SKID) extension' do
+    ca = CA.new 'MyCA'
+
+    subject_key_identifier = ca
+      .certificate
+      .extensions
+      .map(&:to_s)
+      .filter { |ext| ext.match?(/subjectKeyIdentifier/) }
+      .first
+    expect(subject_key_identifier.match?(/[0-9A-F]{2}(:[0-9A-F]{2}){19}/)).to be_truthy
+  end
+
+  it 'has a certificate with an extension that allows its usage as a CA' do
+    ca = CA.new 'MyCA'
+
+    basicConstraints = ca
+      .certificate
+      .extensions
+      .map(&:to_s)
+      .filter { |ext| ext.match?(/basicConstraints/) }
+      .first
+    expect(basicConstraints.match?(/critical, CA:TRUE/)).to be_truthy
+  end
+
+  it "has a certificate with an extensions that indicates the CA's key can be used to verify signatures" do
+    ca = CA.new 'MyCA'
+
+    basicConstraints = ca
+      .certificate
+      .extensions
+      .map(&:to_s)
+      .filter { |ext| ext.match?(/keyUsage/) }
+      .first
+    expect(basicConstraints.match?(/critical, Certificate Sign, CRL Sign/)).to be_truthy
+  end
+
+  it 'has a self signed certificate' do
+    ca = CA.new 'MyCA'
+
+    expect(ca.certificate.verify(ca.certificate.public_key)).to be_truthy
   end
 end
